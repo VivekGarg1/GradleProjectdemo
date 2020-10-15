55f45dc0fc466b9a7b9b883b25d375218a6e0553
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-10-15 08:13:09 +0530

    code merge

---
+++ GradleProjectdemo/src/main/java/com/home/App.java
@@ -0,0 +1,11 @@
+package com.home;
+
+public class App {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+}
